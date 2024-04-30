df54dcf0ee96a7420d00c9b4e12a6da42ce92036
abhithgowda <abhithgowdaa@gmail.com>
2024-04-30 22:48:57 +0530

    deployment correction

--- main.py
+++
@@ -1,6 +0,0 @@
-from Website import create_app
-
-app = create_app()
-if __name__ == "__main__":
-    # whenever changes are made to the python code , the server reruns --> debug=True
-    app.run(debug=True)# made changes
